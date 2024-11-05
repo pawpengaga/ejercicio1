6e7351cdb7fd26fdbd06f8ee9f667a8af0216b71
Erick Rivera <yomellamoerick.771@gmail.com>
2024-11-04 21:18:19 -0300

    servicio crud de empleado listo por ahora

--- src/main/java/services/EmpleadoService.java
+++ src/main/java/services/EmpleadoService.java
@@ -24,7 +24,15 @@ public class EmpleadoService implements IEmpleado {
 
   @Override
   public Empleado obtenerEmpleado(String rut) {
-    return null;
+    // Buscamos al empleado y lo almacenamos
+    Empleado empleadoBuscado = empleados.get(rut);
+    // Si no existe es null, y si es null retornamos eso
+    // Si no es null hay un empleado, lo retornamos
+    if(empleadoBuscado == null){
+      System.out.println("El empleado no existe...");
+      return null;
+    }
+    return empleadoBuscado;
   }
 
   @Override
@@ -34,10 +42,29 @@ public class EmpleadoService implements IEmpleado {
 
   @Override
   public void actualizarEmpleado(Empleado empleado) {
+    // Lo que entrara es un empleado con distintos datos pero con el mismo RUT
+    // Lo almacenamos en una variable solo con fines de validacion
+    Empleado nuevoEmpleado = empleados.get(empleado.getRut());
+
+    if (nuevoEmpleado == null) {
+      System.out.println("El empleado que busca actualizar no existe...");
+    } else {
+      // Debido al comportamiendo del hashmap, si una misma clave se busca poner 2 veces, el valor mas reciente la va a sobre escrbir
+      // Hace solo el update
+      empleados.put(empleado.getRut(), empleado);
+    }
+
+
   }
 
   @Override
   public void eliminarEmpleado(String rut) {
+    if(empleados.containsKey(rut)){
+      empleados.remove(rut);
+      System.out.println("Empleado ELIMINADO exitosamente!!");
+    } else {
+      System.out.println("El ID del empleado que busca no figura en nuestra base de datos...");
+    }
   }
 
 
